abfbb75ca1c23be33a6c4766f8903a5f1ecd17fd
theBappy <abappy575@gmail.com>
2025-03-05 13:15:13 +0600

    modified package.json start command

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ import cookieParser from 'cookie-parser'
 import dotenv from 'dotenv'
 import path from 'path'
 import cors from 'cors'
-
+import mongoose from 'mongoose'
 
 // Files 
 import connectDB from './config/db.js'
@@ -13,26 +13,20 @@ import genreRoutes from './routes/genreRoutes.js'
 import moviesRoutes from './routes/moviesRoutes.js'
 import uploadRoutes from './routes/uploadRoutes.js'
 
-
-
 // Configuration
-dotenv.config()
-connectDB()
-
+dotenv.config() // Load environment variables from .env file
+connectDB() // Connect to MongoDB
 
 const app = express()
 
-
 // Middlewares
 app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 
+app.use(cors())
 
-app.use(cors());
-
-
-
+// Use your environment variable for the port
 const PORT = process.env.PORT || 3000
 
 // Routes
@@ -41,13 +35,19 @@ app.use('/api/v1/genre', genreRoutes)
 app.use('/api/v1/movies', moviesRoutes)
 app.use('/api/v1/upload', uploadRoutes)
 
+// Serving static files from the "uploads" folder
 const __dirname = path.resolve()
-app.use('/uploads', express.static(path.join(__dirname + '/uploads')))
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
 
+// MongoDB connection
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => console.log('Successfully connected to MongoDB'))
+  .catch((err) => console.log('Error connecting to MongoDB:', err))
 
-
-app.listen(PORT, ()=> console.log(`Server is running on port ${PORT}`))
-
-
-
-
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
+})
